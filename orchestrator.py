f7ed2503a4ea9af44ab5d775fb3a99235f8ed7e2
Matteo Mancini <matteo.mancini@pegaso2000.it>
2025-09-27 14:45:15 +0200

    Aggiunto un task in coda al wf

--- orchestrator.py
+++ orchestrator.py
@@ -7,17 +7,24 @@ def extract():
 
 @task(name="Process Data Task 2")
 def process_data(data):
-    print("Processing data...")
+    print("Processing data 2 ...")
     # Simulate data processing
     return [d * 2 for d in data]
 
+@task(name="Process Data Task 3")
+def finalize_data(data):
+    print("Finalizing data 3...")
+    # Simulate data processing
+    return [d / 2 for d in data]
+
 @flow(name="Data Processing Flow")
 def main_flow():
-    print
+    print("Starting data processing flow...")
     raw_data = extract()
     processed_data = process_data(raw_data["data"])
+    finalized_data = finalize_data(processed_data)
     print("Data processing complete.")
-    return processed_data
+    return finalized_data
 
 if __name__ == "__main__":
     result = main_flow()
